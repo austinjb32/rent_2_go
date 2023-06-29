d70cb6a1dac9b87e619576bff303a2cfc611dccb
austinjb32 <austinjb32@gmail.com>
2023-06-29 11:45:32 +0530

    Sixth Commit

--- lib/models/pro.dart
+++ lib/models/pro.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:rent_2_go/models/pro.dart';
 import '../constants.dart';
 import '../screens/details/details_screen.dart';
 import '../screens/home/components/categories.dart';
